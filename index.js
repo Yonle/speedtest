141094359547dae4994fd19a03fd114001846485
yonle <fledyonle@gmail.com>
2021-05-03 10:16:09 +0800

    Without /dev/zero

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const { createReadStream } = require("fs");
-console.log("[Module] Loaded fs");
 const { createServer } = require("http");
-console.log("[Module] Loaded http module");
 const WebSocket = require("ws");
-console.log("[Module] Loaded ws module");
 const express = require("express");
-console.log("[Module] Loaded express module");
 const app = express();
-console.log("[Express] Created app");
+const randomData = require("crypto").randomBytes(64000);
 const server = createServer(app);
-console.log("[Server] Server created");
 
 const listener = server.listen(process.env.PORT, () => {
   console.log("Now listening on port:", listener.address().port);
@@ -27,17 +22,12 @@ wss.on("connection", (ws, req) => {
       ws.close();
     }, 8000);
   }
-  let duplex = WebSocket.createWebSocketStream(ws);
-  let stream = createReadStream("/dev/zero").pipe(duplex);
-  let mb = 0;
+  let int = setInterval(() => ws.send(randomData));
   setTimeout(() => {
     if (ws.readyState == 2) return;
-    stream.destroy();
+    clearInterval(int);
     ws.close();
   }, 8000);
-
-  ws.on("close", () => {
-    stream.destroy();
-  });
-  duplex.on("error", () => stream.destroy());
+  ws.on('close', () => clearInterval(int));
+  ws.on('error', () => ws.close());
 });
